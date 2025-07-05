56b04a15fe8aea5a136e044212e06d0ef9f55489
Athapaththu <sakila.atapattu@gmail.com>
2025-07-05 14:46:53 +0530

    login page add nav bar

--- frontend/src/Pages/loginpage.jsx
+++ frontend/src/Pages/loginpage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Logincard from '../Components/Login/Logincard.jsx';
+import React from "react";
+import Box from "@mui/material/Box";
+import Logincard from "../Components/Login/Logincard.jsx";
+import HomePageNavbar from "../Components/Navbar/Homepagenavbar.jsx";
 
 export default function loginpage() {
   return (
     <Box>
-        <Logincard/>
+      <HomePageNavbar />
+      <Logincard />
     </Box>
-  )
+  );
 }
--- frontend/src/Pages/signuppage.jsx
+++ frontend/src/Pages/signuppage.jsx
@@ -1,11 +1,12 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import Signup from '../Components/Registration/Signupcard.jsx'
-
+import { Box } from "@mui/material";
+import React from "react";
+import Signup from "../Components/Registration/Signupcard.jsx";
+import HomePageNavbar from "../Components/Navbar/Homepagenavbar.jsx";
 export default function signuppage() {
   return (
     <Box>
-        <Signup/>
+      <HomePageNavbar />
+      <Signup />
     </Box>
-  )
+  );
 }
